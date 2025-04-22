e2ef1d01569e78a8bfa2b3fdc8bb95840e8acd05
hopinghan <hopinghan24@gmail.com>
2025-04-22 22:35:40 +0800

    Update item.h

--- item.h
+++ item.h
@@ -1,6 +1,17 @@
 #ifndef ITEM_H
 #define ITEM_H
-
+#include <iostream>
+#include <string>
+using namespace std;
+class Item {
+private:
+	string name;
+public:
+	Item();
+	Item(string );
+	string getName();
+	void print();
+};
 
 
 #endif
