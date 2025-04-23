ab7dd1b437141ec8b9fda31ccf87c36997cc3b1c
hopinghan <hopinghan24@gmail.com>
2025-04-23 23:32:39 +0800

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "item.h"
+#include "time.h"
+#include "money.h"
+using namespace std;
+
+double sum(double summoney[], int size) {
+
+}
+
+int main() {
+	
+}
