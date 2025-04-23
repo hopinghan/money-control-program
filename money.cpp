6724e69d79764c942a7e2ebcc9d4a6190ef508c9
hopinghan <hopinghan24@gmail.com>
2025-04-23 23:32:22 +0800

    Update money.cpp

---
+++ money.cpp
@@ -0,0 +1,20 @@
+#include "money.h"
+Money::Money(){}
+Money::Money(double m) {
+	money = m;
+}
+void Money::setMoney(double m) {
+	this->money = money;
+}
+double Money::getMoney() {
+	return money;
+}
+double Money::GetTotal() {
+	int total = 0;
+	total += money;
+	return total;
+}
+void Money::print() {
+	cout << fixed << setprecision(1);
+	cout << "$=" << money<<endl;
+}
