9d6e27b4ef33a178459245ef4d1130cdd4f2ede9
hopinghan <hopinghan24@gmail.com>
2025-04-23 23:31:12 +0800

    Update time.h

--- time.h
+++ time.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef TIME_H
+#define TIME_H
+#include <iostream>
+using namespace std;
+class Date {
+private:
+	int month;
+	int day;
+public:
+	Date();
+	Date(int ,int  );
+	void print();
+};
+
+
+#endif
+
