4de1abe21c1f5f41bd86bdc7a1e4b6f054ec8b7b
hopinghan <hopinghan24@gmail.com>
2025-04-23 23:32:02 +0800

    Update money.h

--- money.h
+++ money.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef MONEY_H
+#define MONEY_H
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Money {
+private:
+	double money;
+public:
+	Money();
+	Money(double );
+	void setMoney(double );
+	double getMoney();
+	double GetTotal();
+	void print();
+};
+
+
+#endif
+
