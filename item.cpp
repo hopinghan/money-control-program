0dd9c1b8553d795b47defd2f4ef7dc29e8d7bcf4
hopinghan <hopinghan24@gmail.com>
2025-04-23 23:30:33 +0800

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,11 +1,14 @@
 #include "item.h"
-Item::Item() {}
-Item::Item(string) {
-
+Item::Item(){}
+Item::Item(string n) {
+	name = n;
+}
+void Item::setName(string n) {
+	this->name = name;
 }
 string Item::getName() {
-
+	return name;
 }
 void Item::print() {
-
+	cout << name << endl;
 }
