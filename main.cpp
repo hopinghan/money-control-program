86a56fef45a4c83e9a6f559d90c3758a9c050bfe
hopinghan <hopinghan24@gmail.com>
2025-04-25 23:22:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,27 @@
 using namespace std;
 
 double sum(double summoney[], int size) {
-
+    double total = 0;
+    for (int i = 0; i < size; i++) {
+        total += summoney[i];
+    }
+    return total;
 }
 
 int main() {
-	
+    Item item("Apple");
+    item.print();
+
+    Money m1(10.0), m2(25.5), m3(14.0);
+    m1.print();
+    m2.print();
+    m3.print();
+
+    double arr[] = { m1.getMoney(), m2.getMoney(), m3.getMoney() };
+    cout << "Total Money: $" << sum(arr, 3) << endl;
+
+    Date date(4, 25);
+    date.print();
+
+    return 0;
 }
